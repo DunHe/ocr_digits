19b08b3c4cb7d9527f92dbc91930f739960b8fa5
jinpengdunhe <lijinpeng@dunhetech.com>
2016-01-25 21:38:56 +0100

    improve digit segmentation

--- python/ocr_digits/reco_digits/recoDigits.py
+++ python/ocr_digits/reco_digits/recoDigits.py
@@ -18,6 +18,7 @@ from ocr_digits.reco_digits.contours import findContours
 from ocr_digits.reco_digits.contours import drawContours
 from ocr_digits.imgproc.binaryProc import thinArray
 from ocr_digits.imgproc.binaryProc import Xihua 
+import copy
 
 
 def cropImage(img, img_x, img_y, img_h, img_w):
@@ -36,34 +37,99 @@ def cropDigits(img, img_x, img_y, img_h, img_w, num_chars, digit_w=28, digit_h=2
 
     cropImg = cv2.GaussianBlur(cropImg, (5, 5), 0)
 
-    cropImg = cv2.adaptiveThreshold(
-        cropImg,
-        255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY, 11, 2)
+    # http://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html#gsc.tab=0
+    ret3, binImg = cv2.threshold(cropImg, 0, 255,
+                                 cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    cropImg = Xihua(cropImg, thinArray)
+#    cropImg = cv2.adaptiveThreshold(
+#        cropImg,
+#        255,
+#        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+#        cv2.THRESH_BINARY, 11, 2)
 
-    cropImg = bitReverse(cropImg)
+#    cv2.imshow('test', binImg)
+#    cv2.waitKey(0)
 
-    cv2.imshow('test', cropImg)
-    cv2.waitKey(0)
+    #cropImg = Xihua(cropImg, thinArray)
 
-    min_start_x, min_char_width, min_col_width = search_char_x(
-            formatDigit(cropImg),
-            num_chars)
+
+#    cv2.imshow('test', binImg)
+#    cv2.waitKey(0)
+
+    binImg = bitReverse(binImg)
+
+    if cv2.__version__[0] == "2":
+        contours, hierarchy = cv2.findContours(
+            copy.deepcopy(binImg),
+            cv2.RETR_TREE,
+            cv2.CHAIN_APPROX_SIMPLE)
+    else:
+        _, contours, hierarchy = cv2.findContours(
+            copy.deepcopy(binImg),
+            cv2.RETR_TREE,
+            cv2.CHAIN_APPROX_SIMPLE)
+
+    
+#    cv2.imshow('test', binImg)
+#    cv2.waitKey(0)
+
+    rects = []
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if w * h > 20 and w > 5 and h > 5:
+            rects.append((w*h, (x, y, w, h)))
+
+    # sorted by area, get the max area rects
+    sorted_rects = sorted(rects, key=lambda x: x[0], reverse=True)
+    if len(sorted_rects) > num_chars:
+        digit_rects = sorted_rects[:num_chars]
+    else:
+        digit_rects = sorted_rects
+
+    # sorted by x from left to right
+    x_rects = []
+    for digit_rect in digit_rects:
+        x = digit_rect[1][0]
+        rect = digit_rect[1]
+        x_rects.append((x, rect))
+
+    left_to_right_rects = sorted(x_rects, key=lambda x: x[0])
+
     digits = []
-
     for i in range(num_chars):
-        left = min_start_x + (min_char_width + min_col_width) * i
-        _digit = cropImage(cropImg, left, 0, img_h, min_char_width)
-
-        _digit = resizeDigit(_digit,
+        if i < len(left_to_right_rects):
+            rect = left_to_right_rects[i][1]
+            _digit = cropImage(binImg, rect[0], rect[1], rect[3],
+                               rect[2])
+            _digit = resizeDigit(_digit,
                              h=digit_w, w=digit_h,
                              thresh='manual', bitwise=False)
-        _digit = formatDigit(_digit,
-                             h=1, w=784,
+            _digit = formatDigit(_digit,
+                             h=1, w=digit_w*digit_h,
                              thresh=False, bitwise=False)
+            digits.append(_digit)
+        else:
+            _digit = np.zeros((digit_w*digit_h))
+            digits.append(_digit)
 
-        digits.append(_digit)
-    return digits
+##    cv2.imshow('test', cropImg)
+##    cv2.waitKey(0)
+#
+#    min_start_x, min_char_width, min_col_width = search_char_x(
+#            formatDigit(binImg),
+#            num_chars)
+#    digits = []
+#
+#    for i in range(num_chars):
+#        left = min_start_x + (min_char_width + min_col_width) * i
+#        _digit = cropImage(binImg, left, 0, img_h, min_char_width)
+#
+#        _digit = resizeDigit(_digit,
+#                             h=digit_w, w=digit_h,
+#                             thresh='manual', bitwise=False)
+#        _digit = formatDigit(_digit,
+#                             h=1, w=784,
+#                             thresh=False, bitwise=False)
+#
+#        digits.append(_digit)
+    return digits, binImg
